6d84582d3dd1cfde51ba0b9ec2cd97cb783984b2
Oleg Gavrish <oleh.gavrish@nixsolutions.com>
2017-05-23 19:06:28 +0300

    Added avatar stub and launcher icon

--- android/Robomatics/app/src/main/java/ua/nure/havrysh/robomatics/utils/ImageUtils.java
+++ android/Robomatics/app/src/main/java/ua/nure/havrysh/robomatics/utils/ImageUtils.java
@@ -1,12 +1,16 @@
 package ua.nure.havrysh.robomatics.utils;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
 public class ImageUtils {
     public static void load(Context context, String uri, ImageView imageView) {
+        if (TextUtils.isEmpty(uri)) {
+            return;
+        }
         Glide.with(context)
                 .load(uri)
                 .asBitmap()
